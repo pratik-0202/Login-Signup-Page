60f4907521434a68f500e75da3168684adeb74d9
pratik-0202 <pratikrajims@gmail.com>
2024-12-06 17:48:02 +0530

    test disabled

--- src/test/java/com/project1/LoginPage/LoginPageApplicationTests.java
+++ src/test/java/com/project1/LoginPage/LoginPageApplicationTests.java
@@ -1,12 +1,15 @@
 package com.project1.LoginPage;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class LoginPageApplicationTests {
 
 	@Test
+	@Disabled
 	void contextLoads() {
 	}
 
